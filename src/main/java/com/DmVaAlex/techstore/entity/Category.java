32fd6adaeaac105dac1ac5cd4ca6a730d234e28a
dimasakali <dimonsakaly96@gmail.com>
2026-03-13 01:33:04 +0200

    add entity classes: Product, Category, User

---
+++ src/main/java/com/DmVaAlex/techstore/entity/Category.java
@@ -0,0 +1,20 @@
+package com.DmVaAlex.techstore.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
---
+++ src/main/java/com/DmVaAlex/techstore/entity/Product.java
@@ -0,0 +1,23 @@
+package com.DmVaAlex.techstore.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stock;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
