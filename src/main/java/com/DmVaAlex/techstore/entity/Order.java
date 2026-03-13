ff3c28ef4fc89107e07707291ca2c902256e72a7
Peicov Valeri <valera.peikov10@gmail.com>
2026-03-13 23:54:35 +0200

    Class Order

---
+++ src/main/java/com/DmVaAlex/techstore/entity/Order.java
@@ -0,0 +1,34 @@
+package com.DmVaAlex.techstore.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.apache.catalina.User;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_products",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+    private double totalPrice;
+}
